ec728b87d67ac41dc089356c70112c0aac0d7d0f
Christopher Crow <0chriscrew0@gmail.com>
2025-06-24 18:54:47 -0500

    add --verbose argument

--- main.py
+++ main.py
@@ -13,11 +13,17 @@ if len(sys.argv) == 1:
     print("Please provide a prompt.")
     sys.exit(1)
 
+user_prompt = sys.argv[1]
+
 messages = [
-    types.Content(role="user", parts=[types.Part(text=sys.argv[1])])
+    types.Content(role="user", parts=[types.Part(text=user_prompt)])
 ]
 
 response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
 
 print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
+
+if "--verbose" in sys.argv:
+    print(f"User prompt: {user_prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
