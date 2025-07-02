f4d7cf190f04fe8a4a6d994c5ed2597b391d88f9
Christopher Crow <0chriscrew0@gmail.com>
2025-07-02 16:03:08 -0500

    add get_files_info

---
+++ functions/get_files_info.py
@@ -0,0 +1,30 @@
+import os
+
+
+def get_files_info(working_directory, directory=None):
+    full_path = os.path.join(working_directory, directory)
+    abs_working_directory = os.path.abspath(working_directory)
+    abs_full_path = os.path.abspath(full_path)
+
+    output = ""
+    if directory == ".":
+        output += "Result for current directory:\n"
+    else:
+        output += f'Result for \'{directory}\':\n'
+    
+    if not abs_full_path.startswith(abs_working_directory):
+        output += f'\tError: Cannot list "{directory}" as it is outside the permitted working directory'
+        return output
+
+    if not os.path.isdir(full_path):
+        output += f'\tError: "{directory}" is not a directory'
+        return output
+    
+    for file in os.listdir(full_path):
+        file_path = os.path.join(full_path, file)
+        output += " - "
+        output += file + ": "
+        output += "file_size=" + str(os.path.getsize(file_path)) + " bytes, "
+        output += "is_dir=" + str(os.path.isdir(file_path)) + "\n"
+
+    return output
---
+++ tests.py
@@ -0,0 +1,16 @@
+# tests.py
+
+import unittest
+from functions.get_files_info import get_files_info
+
+
+class TestGetFileInfo(unittest.TestCase):
+    def test_get_file_info(self):
+        print(get_files_info("calculator", "."))
+        print(get_files_info("calculator", "pkg"))
+        print(get_files_info("calculator", "/bin"))
+        print(get_files_info("calculator", "../"))
+
+
+if __name__ == "__main__":
+    unittest.main()
