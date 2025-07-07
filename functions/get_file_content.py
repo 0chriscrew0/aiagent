92650c5e6cd3a9efb96688954e52eaf1de588ea2
Christopher Crow <0chriscrew0@gmail.com>
2025-07-07 15:33:14 -0500

    add get_file_content

---
+++ functions/get_file_content.py
@@ -0,0 +1,31 @@
+import os
+
+from .config import MAX_CHARS
+
+
+def get_file_content(working_directory, file_path):
+    full_path = os.path.join(working_directory, file_path)
+    abs_working_directory = os.path.abspath(working_directory)
+    abs_full_path = os.path.abspath(full_path)
+
+    output = ""
+    
+    if not abs_full_path.startswith(abs_working_directory):
+        output += f'\tError: Cannot list "{file_path}" as it is outside the permitted working directory'
+        return output
+
+    if not os.path.isfile(full_path):
+        output += f'\tError: File not found or is not a regular file: "{file_path}"'
+        return output
+    
+    with open(full_path, "r") as f:
+        file_content_string = f.read(MAX_CHARS)
+
+    output += file_content_string
+
+    if os.path.getsize(full_path) > MAX_CHARS:
+        output += f'[...File "{file_path}" truncated at 10000 characters]'
+
+    return output
+
+    
--- tests.py
+++ tests.py
@@ -2,15 +2,15 @@
 
 import unittest
 from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
 
 class TestGetFileInfo(unittest.TestCase):
     def test_get_file_info(self):
-        print(get_files_info("calculator", "."))
-        print(get_files_info("calculator", "pkg"))
-        print(get_files_info("calculator", "/bin"))
-        print(get_files_info("calculator", "../"))
-
+        print(get_file_content("calculator", "main.py"))
+        print(get_file_content("calculator", "pkg/calculator.py"))
+        print(get_file_content("calculator", "/bin/cat"))
+        
 
 if __name__ == "__main__":
     unittest.main()
