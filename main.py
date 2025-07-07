6434bde322b2e3e6ac64473c290a2ea069a63473
Christopher Crow <0chriscrew0@gmail.com>
2025-07-07 17:41:13 -0500

    add function declaration functionality

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+from config import system_prompt
+from functions.get_files_info import available_functions
+
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
-
-system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
 
 if len(sys.argv) == 1:
     print("Please provide a prompt.")
@@ -24,10 +25,16 @@ messages = [
 response = client.models.generate_content(
     model="gemini-2.0-flash-001",
     contents=messages,
-    config=types.GenerateContentConfig(system_instruction=system_prompt)
+    config=types.GenerateContentConfig(
+        tools=[available_functions], system_instruction=system_prompt
+    )
 )
 
-print(response.text)
+if response.function_calls:
+    for function_call_part in response.function_calls:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+else:
+    print(response.text)
 
 if "--verbose" in sys.argv:
     print(f"User prompt: {user_prompt}")
